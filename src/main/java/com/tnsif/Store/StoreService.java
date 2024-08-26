cb2a3ceeea1afbf9d090b8c707cc65385efd207f
Admin <Admin@DESKTOP-HKJU8LV>
2024-08-26 10:17:01 +0530

    fourth commit

---
+++ src/main/java/com/tnsif/Store/StoreService.java
@@ -0,0 +1,35 @@
+package com.tnsif.Store;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+@Service
+@Transactional
+public class StoreService {
+	
+	@Autowired
+	   private StoreRepository repo;
+		public List<Store>listAll()
+		{
+			return repo.findAll();
+		}
+		public Store get(Integer storeid) {
+			
+			return repo.findById (storeid).get();
+		}
+		public void save(Store store) {
+			 repo.save(store);
+			
+		}
+		public void delete(Integer storeid) 
+		{
+			repo.deleteById(storeid);
+			
+		}
+
+
+}
